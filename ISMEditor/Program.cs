cfe61aa61f157bdbc983534342da4fc174210472
nyjin <yjnoh@adtkorea.com>
2016-05-03 11:40:13 +0900

    도움말 변경

--- ISMEditor/Program.cs
+++ ISMEditor/Program.cs
@@ -36,10 +36,10 @@ namespace ISMEditor
         [Option('v', "version", Required = true, HelpText = "대상 버전을 지정합니다.")]
         public string Version { get; set; }
 
-        [Option('k', "package", Required = false, HelpText = "패키지 코드를 지정합니다.")]
+        [Option('k', "package", Required = false, HelpText = "패키지 코드를 지정합니다. 지정하지 않으면 자동 생성 됩니다.")]
         public string PackageCode { get; set; }
 
-        [Option('d', "product", Required = false, HelpText = "제품 코드를 지정합니다.")]
+        [Option('d', "product", Required = false, HelpText = "제품 코드를 지정합니다. 지정하지 않으면 자동 생성 됩니다.")]
         public string ProductCode { get; set; }
 
 
